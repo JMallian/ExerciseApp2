3a546fe1b2b8dab5450a92199c1df666183236b8
JMallian <jlmallian@gmail.com>
2017-11-13 11:12:14 -0600

    some work on using a UIPickerView to choose muscle groups for an exercise

--- ExerciseApp2/addExerciseViewController.swift
+++ ExerciseApp2/addExerciseViewController.swift
@@ -8,15 +8,42 @@
 
 import UIKit
 
-class addExerciseViewController: UIViewController {
+class addExerciseViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
     let muscleTextfield: UITextField
+    let musclePicker: UIPickerView
     let goBackButton: UIButton
-    let muscles: ExerciseGroup
+    //let muscles: ExerciseGroup
+    let pickerViewData: [String]
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         goBackButton = UIButton()
         muscleTextfield = UITextField()
-        muscles = ExerciseGroup()
+        musclePicker = UIPickerView()
+        //muscles = ExerciseGroup()
+        pickerViewData = [
+            "quadriceps",
+            "gluteals",
+            "hamstrings",
+            "erectors",
+            "upper gluteals",
+            "deltoids",
+            "pectorals",
+            "triceps",
+            //"upper trapezius",
+            //"anterior deltoids",
+            "lats",
+            "midtraps",
+            "rhomboids",
+            "biceps",
+            //"posterior deltoids",
+            //"lateral deltoids",
+            "traps",
+            "rectus abdominis",
+            "internal obliques",
+            "external obliques",
+            "calves"
+        ]
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
         //component placement
@@ -25,6 +52,12 @@ class addExerciseViewController: UIViewController {
         let centerY: CGFloat = screenSize.height / 2
         goBackButton.frame = CGRect(x: 0, y: centerY + 125, width: screenSize.width, height: 50)
         muscleTextfield.frame = CGRect(x: 0, y: centerY + 25, width: screenSize.width, height: 50)
+        
+        //pickerView stuff
+        musclePicker.delegate = self
+        musclePicker.dataSource = self
+        muscleTextfield.inputView = musclePicker
+        
         
         //button titles
         goBackButton.setTitle("Go Back", for: .normal)
@@ -65,5 +98,16 @@ class addExerciseViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerViewData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerViewData[row]
+    }
     
 }
