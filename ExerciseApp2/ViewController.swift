c208b5ec4ba3f16e254d3438c456e5d7befb5fc4
JMallian <jlmallian@gmail.com>
2017-11-13 08:10:11 -0600

    made buttons and such for main page

--- ExerciseApp2/ViewController.swift
+++ ExerciseApp2/ViewController.swift
@@ -9,7 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let startNewButton: UIButton
+    let historyButton: UIButton
+    let createButton: UIButton
+    let addExerciseButton: UIButton
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        startNewButton = UIButton()
+        historyButton = UIButton()
+        createButton = UIButton()
+        addExerciseButton = UIButton()
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        //button placement
+        let screenSize: CGSize = UIScreen.main.bounds.size
+        //let centerX: CGFloat = screenSize.width / 2
+        let centerY: CGFloat = screenSize.height / 2
+        startNewButton.frame = CGRect(x: 0, y: centerY - 175, width: screenSize.width, height: 50)
+        historyButton.frame = CGRect(x: 0, y: centerY - 75, width: screenSize.width, height: 50)
+        createButton.frame = CGRect(x: 0, y: centerY + 25, width: screenSize.width, height: 50)
+        addExerciseButton.frame = CGRect(x: 0, y: centerY + 125, width: screenSize.width, height: 50)
+        
+        //button titles
+        startNewButton.setTitle("Start New Workout", for: .normal)
+        historyButton.setTitle("History", for: .normal)
+        createButton.setTitle("Create Workout", for: .normal)
+        addExerciseButton.setTitle("Add Exercise", for: .normal)
+        
+        //button colors
+        startNewButton.backgroundColor = UIColor.cyan
+        historyButton.backgroundColor = UIColor.cyan
+        createButton.backgroundColor = UIColor.cyan
+        addExerciseButton.backgroundColor = UIColor.cyan
+        
+        startNewButton.setTitleColor(UIColor.black, for: .normal)
+        historyButton.setTitleColor(UIColor.black, for: .normal)
+        createButton.setTitleColor(UIColor.black, for: .normal)
+        addExerciseButton.setTitleColor(UIColor.black, for: .normal)
+        
+        //button functions
+        addExerciseButton.addTarget(self, action: #selector(ViewController.addExercisePressed), for: UIControlEvents.touchUpInside)
+        
+        self.view.addSubview(startNewButton)
+        self.view.addSubview(historyButton)
+        self.view.addSubview(createButton)
+        self.view.addSubview(addExerciseButton)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +69,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func addExercisePressed() {
+        self.present(addExerciseViewController(), animated: true, completion: nil)
     }
 
 
