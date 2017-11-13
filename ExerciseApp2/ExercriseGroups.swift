8e3d12ada40153b025019864f902b4d4b90465c2
JMallian <jlmallian@gmail.com>
2017-11-13 09:08:09 -0600

    made class to hold muscle groups

---
+++ ExerciseApp2/ExercriseGroups.swift
@@ -0,0 +1,35 @@
+//
+//  ExercriseGroups.swift
+//  ExerciseApp2
+//
+//  Created by Mallian, Jessica L on 11/13/17.
+//  Copyright © 2017 Mallian, Jessica L. All rights reserved.
+//
+
+import Foundation
+
+class ExerciseGroup {
+    let muscleGroups = [
+        "quadriceps",
+        "gluteals",
+        "hamstrings",
+        "erectors",
+        "upper gluteals",
+        "deltoids",
+        "pectorals",
+        "triceps",
+        //"upper trapezius",
+        //"anterior deltoids",
+        "lats",
+        "midtraps",
+        "rhomboids",
+        "biceps",
+        //"posterior deltoids",
+        //"lateral deltoids",
+        "traps",
+        "rectus abdominis",
+        "internal obliques",
+        "external obliques",
+        "calves"
+    ]
+}
