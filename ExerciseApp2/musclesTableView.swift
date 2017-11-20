341827f829c3435a0813d26e466821276e2eb3b9
JMallian <jlmallian@gmail.com>
2017-11-19 20:24:02 -0600

    more work on MusclesTableView, cannot add empty spaces and have it save as a muscle

--- ExerciseApp2/musclesTableView.swift
+++ ExerciseApp2/musclesTableView.swift
@@ -18,6 +18,7 @@ class musclesTableView: UITableViewController {
     var checkMarkCount: Int = 0
     var maxCheckMarkCount: Int = 3
     var dataToPass: [String] = []
+    let headerHeight: CGFloat = 50
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,8 +67,6 @@ class musclesTableView: UITableViewController {
         if myCell?.accessoryType == .checkmark {
             myCell?.accessoryType = .none
             checkMarkCount = checkMarkCount - 1
-            //remove it from the array
-            //this could probably be done better - possibly with a dictionary where key = indexPath
             for item in dataToPass {
                 if item == myCell?.textLabel?.text {
                     dataToPass.remove(at: dataToPass.index(of: (myCell?.textLabel?.text)!)!)
@@ -88,30 +87,19 @@ class musclesTableView: UITableViewController {
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let frame: CGRect = tableView.frame
-        
-        //need to make it more obvious that there are buttons here by adding some space between them
-        let editButton = UIButton()
-        editButton.frame = CGRect(x: 0, y: 0, width: frame.size.width/2, height: 50)
-        editButton.setTitle("Edit Muscle Groups", for: .normal)
-        editButton.backgroundColor = ColorsForApp.componentBackgroundColor
-        editButton.setTitleColor(ColorsForApp.textColor, for: .normal)
-        editButton.addTarget(self, action: #selector(musclesTableView.editButtonPressed), for: UIControlEvents.touchUpInside)
-        let addButton = UIButton()
-        addButton.frame = CGRect(x: frame.size.width/2, y: 0, width: frame.size.width/2, height: 50)
-        addButton.setTitle("Add Muscle Group", for: .normal)
-        addButton.backgroundColor = ColorsForApp.componentBackgroundColor
-        addButton.setTitleColor(ColorsForApp.textColor, for: .normal)
-        addButton.addTarget(self, action: #selector(musclesTableView.addButtonPressed), for: UIControlEvents.touchUpInside)
         let myHeaderView: UIView = UIView()
         myHeaderView.frame = CGRect(x: 0, y: 0, width: frame.size.height, height: frame.size.width)
         myHeaderView.backgroundColor = UIColor.red
-        myHeaderView.addSubview(editButton)
-        myHeaderView.addSubview(addButton)
+        
+        addEditButtonToHeader(view: myHeaderView, frame: frame)
+        addAddButtonToHeader(view: myHeaderView, frame: frame)
+        addOrderButtonToHeader(view: myHeaderView, frame: frame)
+        
         return myHeaderView
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
+        return headerHeight
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
@@ -131,34 +119,70 @@ class musclesTableView: UITableViewController {
     @objc func editButtonPressed(sender: UIButton) {
         if isEditing {
             setEditing(false, animated: true)
-            sender.setTitle("Edit Muscle Groups", for: .normal)
+            sender.setTitle("Edit", for: .normal)
         }else{
             setEditing(true, animated: true)
-            sender.setTitle("Done Editing", for: .normal)
+            sender.setTitle("Done", for: .normal)
         }
     }
     
-    
-    //
-    //
-    //
-    // to do: check that it's not an empty String before adding and give a go back or whatever option in alert
     @objc func addButtonPressed() {
         let alert = UIAlertController(title: "Add Muscle", message: "Type Muscle to Add", preferredStyle: .alert)
         alert.addTextField { (textField) in
             //textField.text = "type muscle here"
+            //configure textfield here
         }
         
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { (_) in
             let textField = alert.textFields![0]
-            //add to array
-            self.dataMuscleGroups.append((textField.text)!) //need to check that it's not an emtpy String
-            //add to tableView
-            //let indexPath = indexPath
-            self.tableView.insertRows(at: [IndexPath(row: self.dataMuscleGroups.count - 1, section: 0)], with: .automatic)
+            
+            if !(textField.text?.trimmingCharacters(in: CharacterSet.whitespaces).isEmpty)! {
+                //add to array
+                self.dataMuscleGroups.append((textField.text)!) //need to check that it's not an emtpy String
+                //add to tableView
+                self.tableView.insertRows(at: [IndexPath(row: self.dataMuscleGroups.count - 1, section: 0)], with: .automatic)
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Go Back", style: .default, handler: { (_) in
+            
         }))
         
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func orderButtonPressed() {
+        
+    }
+    
+    func addEditButtonToHeader(view: UIView, frame: CGRect) {
+        let editButton = UIButton()
+        editButton.frame = CGRect(x: 0, y: 0, width: frame.size.width/2, height: headerHeight)
+        editButton.setTitle("Edit", for: .normal)
+        editButton.backgroundColor = ColorsForApp.componentBackgroundColor
+        editButton.setTitleColor(ColorsForApp.textColor, for: .normal)
+        editButton.addTarget(self, action: #selector(musclesTableView.editButtonPressed), for: UIControlEvents.touchUpInside)
+        view.addSubview(editButton)
+    }
+    
+    func addAddButtonToHeader(view: UIView, frame: CGRect) {
+        let addButton = UIButton()
+        addButton.frame = CGRect(x: (frame.size.width/3) + (frame.size.width/3), y: 0, width: frame.size.width/3, height: headerHeight)
+        addButton.setTitle("Add", for: .normal)
+        addButton.backgroundColor = ColorsForApp.componentBackgroundColor
+        addButton.setTitleColor(ColorsForApp.textColor, for: .normal)
+        addButton.addTarget(self, action: #selector(musclesTableView.addButtonPressed), for: UIControlEvents.touchUpInside)
+        view.addSubview(addButton)
+    }
+    
+    func addOrderButtonToHeader(view: UIView, frame: CGRect) {
+        let orderMusclesButton = UIButton()
+        orderMusclesButton.frame = CGRect(x: frame.size.width/3, y: 0, width: frame.size.width/3, height: headerHeight)
+        orderMusclesButton.setTitle("Order", for: .normal)
+        orderMusclesButton.backgroundColor = ColorsForApp.componentBackgroundColor
+        orderMusclesButton.setTitleColor(ColorsForApp.textColor, for: .normal)
+        orderMusclesButton.addTarget(self, action: #selector(musclesTableView.orderButtonPressed), for: UIControlEvents.touchUpInside)
+        view.addSubview(orderMusclesButton)
     }
 }
 
