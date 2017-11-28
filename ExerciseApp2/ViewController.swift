1c08ba1ae6a1dd50724432eac657b996a5c8546e
JMallian <jlmallian@gmail.com>
2017-11-27 18:16:22 -0600

    created constraints in ViewController that actually work the way I want them to (that is, they resize when phone is rotated)! still need to pretty the up though

--- ExerciseApp2/ViewController.swift
+++ ExerciseApp2/ViewController.swift
@@ -68,42 +68,66 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         // I did all this and I still don't know how to get the buttons to resize when the device is rotated
         // but at least it looks the same on all screen sizes 
-        let navBarHeight = self.navigationController?.navigationBar.frame.height
-        let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
-        var viewHeight: CGFloat
-        if let unwrapped = navBarHeight { //is this even necessary?
-            viewHeight = UIScreen.main.bounds.height - unwrapped - statusBarHeight
-        }else{
-            viewHeight = UIScreen.main.bounds.height
-        }
-        let buttonHeight: CGFloat = viewHeight * 0.20
-        let spaceBetweenButtons: CGFloat = viewHeight * 0.04
-        let buttonBothSidePadding: CGFloat = 10
-        let topMostButtonPadding: CGFloat = viewHeight * 0.04 //this is only used once it's just nice to have it here
-        
+//        let navBarHeight = self.navigationController?.navigationBar.frame.height
+//        let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
+//        var viewHeight: CGFloat
+//        if let unwrapped = navBarHeight { //is this even necessary?
+//            viewHeight = UIScreen.main.bounds.height - unwrapped - statusBarHeight
+//        }else{
+//            viewHeight = UIScreen.main.bounds.height
+//        }
+//        let buttonHeight: CGFloat = viewHeight * 0.20
+//        let spaceBetweenButtons: CGFloat = viewHeight * 0.04
+//        let buttonBothSidePadding: CGFloat = 10
+//        let topMostButtonPadding: CGFloat = viewHeight * 0.04 //this is only used once it's just nice to have it here
+//
+//        self.view.addSubview(startNewButton)
+//        startNewButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 8).isActive = true
+//        startNewButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+//        startNewButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -0).isActive = true
+//        startNewButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+//
+//        self.view.addSubview(historyButton)
+//        historyButton.topAnchor.constraint(equalTo: startNewButton.bottomAnchor, constant: 8).isActive = true
+//        historyButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+//        historyButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -0).isActive = true
+//        historyButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+//
+//        self.view.addSubview(createButton)
+//        createButton.topAnchor.constraint(equalTo: historyButton.bottomAnchor, constant: 8).isActive = true
+//        createButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+//        createButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -0).isActive = true
+//        createButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+//
+//        self.view.addSubview(addExerciseButton)
+//        addExerciseButton.topAnchor.constraint(equalTo: createButton.bottomAnchor, constant: 8).isActive = true
+//        addExerciseButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+//        addExerciseButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -0).isActive = true
+//        addExerciseButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        let margins = view.layoutMarginsGuide
         self.view.addSubview(startNewButton)
-        startNewButton.topAnchor.constraint(equalTo: view.topAnchor, constant: topMostButtonPadding).isActive = true
-        startNewButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: buttonBothSidePadding).isActive = true
-        startNewButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -buttonBothSidePadding).isActive = true
-        startNewButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        startNewButton.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        startNewButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        startNewButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.20).isActive = true
+        startNewButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
         
         self.view.addSubview(historyButton)
-        historyButton.topAnchor.constraint(equalTo: startNewButton.bottomAnchor, constant: spaceBetweenButtons).isActive = true
-        historyButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: buttonBothSidePadding).isActive = true
-        historyButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -buttonBothSidePadding).isActive = true
-        historyButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        historyButton.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        historyButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        historyButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.20).isActive = true
+        historyButton.topAnchor.constraint(equalTo: startNewButton.bottomAnchor, constant: 10).isActive = true
         
         self.view.addSubview(createButton)
-        createButton.topAnchor.constraint(equalTo: historyButton.bottomAnchor, constant: spaceBetweenButtons).isActive = true
-        createButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: buttonBothSidePadding).isActive = true
-        createButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -buttonBothSidePadding).isActive = true
-        createButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        createButton.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        createButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        createButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.20).isActive = true
+        createButton.topAnchor.constraint(equalTo: historyButton.bottomAnchor, constant: 10).isActive = true
         
         self.view.addSubview(addExerciseButton)
-        addExerciseButton.topAnchor.constraint(equalTo: createButton.bottomAnchor, constant: spaceBetweenButtons).isActive = true
-        addExerciseButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: buttonBothSidePadding).isActive = true
-        addExerciseButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -buttonBothSidePadding).isActive = true
-        addExerciseButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        addExerciseButton.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        addExerciseButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        addExerciseButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.20).isActive = true
+        addExerciseButton.topAnchor.constraint(equalTo: createButton.bottomAnchor, constant: 10).isActive = true
     }
     
     override func didReceiveMemoryWarning() {
